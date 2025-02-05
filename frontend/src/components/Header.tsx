aeb4610d39b455ce24a17a52c65012f9d7c4232b
Nanoruri <kaby1217@gmail.com>
2025-02-05 17:08:52 +0900

    feat: 헤더 로고 파일 추가
    - 헤더에 적용할 로고 이미지(zenlessLog.png)파일 추가
    - 로고 이미지 헤더 컴포넌트에 적용
    - 헤더 스타일 수정
    - .header-logo컨테이너를 추가하여 로고 이미지에 대한 스타일 설정 수정 가능

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,8 +1,16 @@
 import React from "react";
 import "../styles/header.css";
+import logo from "../public/zenlessLogo.png";
+
 
 const Header: React.FC = () => {
-    return <header className="header">Zenless-Calculator</header>;
+
+    return (
+        <header className="header" >
+            <img src={logo} alt="Zenless Logo" className="header-logo" />
+            <span>Zenless-Calculator</span>
+        </header>
+    );
 };
 
 export default Header;
