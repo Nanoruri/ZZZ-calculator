2a54ad0b1aa2e006fad76dec179fcd05ed5e1416
Nanoruri <kaby1217@gmail.com>
2025-02-05 15:22:32 +0900

    chore: 스프링 애플리케이션 진입점 추가
    - ZenlessCalcApplication.java 추가

---
+++ backend/src/main/java/me/jh/zenless/calc/ZenlessCalcApplication.java
@@ -0,0 +1,13 @@
+package me.jh.zenless.calc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZenlessCalcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZenlessCalcApplication.class, args);
+    }
+
+}
