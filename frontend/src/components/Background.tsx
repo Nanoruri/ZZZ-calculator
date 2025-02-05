dab2edfc8e62a747ace26bd9f17a4100d7d80dbe
Nanoruri <kaby1217@gmail.com>
2025-02-05 15:39:19 +0900

    feat: 메인 페이지 UI 컴포넌트 추가
    - Background.tsx: 배경 스타일 적용을 위한 컴포넌트 추가
    - CharacterSelect.tsx: 캐릭터 선택 UI 추가
    - Header.tsx: 애플리케이션 헤더 추가
    - 관련 CSS 파일 추가

---
+++ frontend/src/components/Background.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "../styles/background.css";
+
+const Background: React.FC = () => {
+    return <div className="background"></div>;
+};
+
+export default Background;
---
+++ frontend/src/components/Header.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "../styles/header.css";
+
+const Header: React.FC = () => {
+    return <header className="header">Zenless-Calculator</header>;
+};
+
+export default Header;
