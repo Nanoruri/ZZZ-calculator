645c91c2d6eda69c7db659bbda6e8f58aa3360a7
Nanoruri <kaby1217@gmail.com>
2025-02-11 12:19:52 +0900

    feat: Repository 추가
    - 육성 재료 데이터를 DB에서 가져오는 ResourceCoreRepository추가
    - @EntityGraph를 추가하여 findAll() 호출 시 추가하여 연관 데이터 함께 조회하도록 설정
    - N+1 문제를 방지하고 성능 최적화
    - 기존 Lazy Loading으로 인해 발생할 수 있는 추가 쿼리 방지

---
+++ backend/src/main/java/me/jh/zenless/calc/repository/agent/ResourceCoreRepository.java
@@ -0,0 +1,16 @@
+package me.jh.zenless.calc.repository.agent;
+
+import me.jh.zenless.calc.entity.material.ResourceCore;
+import org.springframework.data.jpa.repository.EntityGraph;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface ResourceCoreRepository extends JpaRepository<ResourceCore, String> {
+	@Override
+	@EntityGraph(attributePaths = {"levelUpResources", "skillResources", "coreSkillResources", "breakthroughResources"})
+	List<ResourceCore> findAll();
+
+}
