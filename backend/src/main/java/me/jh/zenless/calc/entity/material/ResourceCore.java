ff46e96b15d8ce8df67733bfef96b6e6b0161c65
Nanoruri <kaby1217@gmail.com>
2025-02-17 23:47:00 +0900

    fix(backend): 육성 재료 저장 시 기존 데이터가 새로운 데이터로 덮어씌워지는 문제 해결
    - ResourceCore엔티티의 orphanRemoval속성 제거로 테이블의 기존 데이터가 새로운 데이터로 덮어씌워지는 문제 해결

--- backend/src/main/java/me/jh/zenless/calc/entity/material/ResourceCore.java
+++ backend/src/main/java/me/jh/zenless/calc/entity/material/ResourceCore.java
@@ -22,19 +22,19 @@ public class ResourceCore {
     @Column(name = "RESOURCE_CORE_ID")
     private String name; // 어디에 필요한 재료인지 ex) 경험치 재료, 스킬 재료, 돌파재료...
 
-    @OneToMany(mappedBy = "resourceCore", fetch = FetchType.LAZY, cascade = CascadeType.ALL, orphanRemoval = true)
+    @OneToMany(mappedBy = "resourceCore", fetch = FetchType.LAZY, cascade = CascadeType.ALL)
     @JsonManagedReference
     private Set<LevelUpResource> levelUpResources = new HashSet<>();
 
-    @OneToMany(mappedBy = "resourceCore", fetch = FetchType.LAZY, cascade = CascadeType.ALL, orphanRemoval = true)
+    @OneToMany(mappedBy = "resourceCore", fetch = FetchType.LAZY, cascade = CascadeType.ALL)
     @JsonManagedReference
     private Set<SkillResource> skillResources = new HashSet<>();
 
-    @OneToMany(mappedBy = "resourceCore", fetch = FetchType.LAZY, cascade = CascadeType.ALL, orphanRemoval = true)
+    @OneToMany(mappedBy = "resourceCore", fetch = FetchType.LAZY, cascade = CascadeType.ALL)
     @JsonManagedReference
     private Set<CoreSkillResource> coreSkillResources = new HashSet<>();
 
-    @OneToMany(mappedBy = "resourceCore", fetch = FetchType.LAZY, cascade = CascadeType.ALL, orphanRemoval = true)
+    @OneToMany(mappedBy = "resourceCore", fetch = FetchType.LAZY, cascade = CascadeType.ALL)
     @JsonManagedReference
     private Set<BreakthroughResource> breakthroughResources = new HashSet<>();
 }
