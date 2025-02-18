0d45b4bbf8ab5d3fa511bb207a05790f3f7ff163
Nanoruri <kaby1217@gmail.com>
2025-02-18 14:34:37 +0900

    build(frontend): 리액트 테스트 라이브러리 추가
    - 리액트 코드 테스트를 위해 jest 라이브러리 추가 및 jest설정 구성

---
+++ frontend/jest.config.ts
@@ -0,0 +1,19 @@
+import type { Config } from "jest";
+
+const config: Config = {
+    preset: "ts-jest",
+    testEnvironment: "jest-environment-jsdom",  // DOM을 사용하는 테스트에 적합
+    transform: {
+        "^.+\\.(ts|tsx)$": ["ts-jest", {
+            tsconfig: {
+                jsx: "react"
+            }
+        }],
+    },
+
+    moduleNameMapper: {
+        "\\.(css|less|scss|sass)$": "identity-obj-proxy", // CSS 파일 처리
+    },
+};
+
+export default config;
