f9b014fc4641cbc3f5a540f02b09862d32eb868c
Nanoruri <kaby1217@gmail.com>
2025-02-18 15:10:23 +0900

    config(frontend): 테스트 코드 실행을 위한 tsconfig.test 프로파일 추가 및 jest 설정
    - 테스트 코드의 실행 설정을 위한 tsconfig.test.json 파일 추가 및 jest파일 수정

--- frontend/jest.config.ts
+++ frontend/jest.config.ts
@@ -4,13 +4,13 @@ const config: Config = {
     preset: "ts-jest",
     testEnvironment: "jest-environment-jsdom",  // DOM을 사용하는 테스트에 적합
     transform: {
-        "^.+\\.(ts|tsx)$": ["ts-jest", {
-            tsconfig: {
-                jsx: "react"
+        "^.+\\.(ts|tsx)$": [
+            "ts-jest",
+            {
+                tsconfig: "./tsconfig.test.json"  // tsconfig.test.json을 적용
             }
-        }],
+        ],
     },
-
     moduleNameMapper: {
         "\\.(css|less|scss|sass)$": "identity-obj-proxy", // CSS 파일 처리
     },
